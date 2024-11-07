6060eb8214148b3056f7e7c2343a53161125a020
aatmik-panse <aatmik.23bcs10082@ms.sst.scaler.com>
2024-11-07 22:14:55 +0530

    Initial Commit

---
+++ LetsDraw/LetsDrawApp.swift
@@ -0,0 +1,32 @@
+//
+//  LetsDrawApp.swift
+//  LetsDraw
+//
+//  Created by Aatmik Panse on 07/11/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LetsDrawApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
